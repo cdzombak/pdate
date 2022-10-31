f939d9d1807bdf996b4b87971e4ed338e9ec0ce1
Chris Dzombak <chris@dzombak.com>
2022-10-31 11:09:27 -0400

    default to now; add -i flag for interactive mode

--- main.go
+++ main.go
@@ -12,16 +12,11 @@ var version = "<dev>"
 
 func main() {
 	var val string
+	var parsed time.Time
+	var err error
 
 	if len(os.Args) < 2 {
-		fmt.Printf("Enter datetime string to parse: ")
-		reader := bufio.NewReader(os.Stdin)
-		input, err := reader.ReadString('\n')
-		fmt.Println("")
-		if err != nil {
-			panic(err)
-		}
-		val = strings.TrimSuffix(input, "\n")
+		parsed = time.Now()
 	} else {
 		val = os.Args[1]
 
@@ -30,13 +25,24 @@ func main() {
 			fmt.Println("usage: dateutil [datetime string]")
 			os.Exit(0)
 		}
-	}
 
-	val = strings.TrimSpace(val)
-	parsed, err := Parse(val)
-	if err != nil {
-		fmt.Println("failed to parse input")
-		os.Exit(1)
+		if val == "-i" || val == "--interactive" {
+			fmt.Printf("Enter datetime string to parse: ")
+			reader := bufio.NewReader(os.Stdin)
+			input, err := reader.ReadString('\n')
+			fmt.Println("")
+			if err != nil {
+				panic(err)
+			}
+			val = strings.TrimSuffix(input, "\n")
+		}
+
+		val = strings.TrimSpace(val)
+		parsed, err = Parse(val)
+		if err != nil {
+			fmt.Println("failed to parse input")
+			os.Exit(1)
+		}
 	}
 
 	localLoc, err := time.LoadLocation("Local")
@@ -51,13 +57,20 @@ func main() {
 	}
 	parsedUTC := parsed.In(utcLoc)
 
-	fmt.Printf(" input:\t%s\n", val)
-	fmt.Printf("parsed:\t%s\n", parsed.Format("2006-01-02 15:04:05 MST"))
-	fmt.Println("       \t(verify this matches your input)")
+	if val != "" {
+		fmt.Printf(" input:\t%s\n", val)
+		fmt.Printf("parsed:\t%s\n", parsed.Format("2006-01-02 15:04:05 MST"))
+		fmt.Println("       \t(verify this matches your input)")
+	} else {
+		fmt.Println(" input:\tnow")
+	}
 	fmt.Println("")
+
 	fmt.Printf("   UTC:\t%s\n", parsedUTC.Format("2006-01-02 3:04:05 PM"))
 	fmt.Printf("       \t%s\n", parsedUTC.Format("2006-01-02T15:04:05Z"))
 	fmt.Println("")
 	fmt.Printf(" local:\t%s\n", parsedLocal.Format("2006-01-02 3:04:05 PM MST"))
-	fmt.Printf("\t(%s)\n", CustomEnglishTimeAgo.Format(parsed))
+	if val != "" {
+		fmt.Printf("\t(%s)\n", CustomEnglishTimeAgo.Format(parsed))
+	}
 }
